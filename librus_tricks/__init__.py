4ec80211ec3b89d7a59da13f087a96e0ae9b5591
Krystian <krystian@postek.eu>
2018-12-29 23:35:13 +0100

    Removed incompatibility

--- librus_tricks/__init__.py
+++ librus_tricks/__init__.py
@@ -1,7 +1,7 @@
 import requests
 
 from librus_tricks.types import SynergiaSessionUser
-from librus_tricks.utils import get_access_token, get_auth_code, get_synergia_users, SYNERGIAAUTHURL
+from librus_tricks.utils import get_access_token, get_auth_code, get_synergia_users, SYNERGIAAUTHURL, try_until_die
 
 name = 'librus_tricks'
 
@@ -12,26 +12,21 @@ def ez_login(email, passwd, rtype='UserObjs'):
     :param email: Email do librus synergia
     :type email: str
     :type passwd: str
-    :param rtype: Wybiera w jaki co ma zostać zwrócone na return, wybieraj między `UserObjs` lub `tokens` lub `raw`
+    :param rtype: Wybiera w jaki co ma zostać zwrócone na return, wybieraj między `UserObjs` lub `tokens`
     :type rtype: str
     """
     auth_code = get_auth_code(email, passwd)
     librus_token = get_access_token(auth_code)
-    response = requests.get(
-        SYNERGIAAUTHURL,
-        headers={'Authorization': f'Bearer {librus_token}'}
-    )
+    response = try_until_die(librus_token)
     if rtype == 'UserObjs':
         users = []
-        for a in response.json()['accounts']:
+        for a in response:
             users.append(SynergiaSessionUser(a))
         return users
     elif rtype == 'tokens':
         librus_tokens = {}
-        for t in response.json()['accounts']:
-            librus_token[t['studentName']] = t['accessToken']
+        for t in response:
+            librus_tokens[t['studentName']] = t['accessToken']
         return librus_token
-    elif rtype == 'raw':
-        return response.text
     else:
         raise KeyError('Zły argument dla rtype')
