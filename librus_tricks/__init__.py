d7c2865d393d379c2f973d4a9cfe5f730bc552b3
Krystian <krystian@postek.eu>
2019-01-01 11:53:33 +0100

    Made many changes in class management
    Need to update README.md

--- librus_tricks/__init__.py
+++ librus_tricks/__init__.py
@@ -1,31 +1,4 @@
-from librus_tricks.types import SynergiaSessionUser
-from librus_tricks.utils import get_auth_code, get_access_token, try_many_times
-
+from .utils import ez_login
+from .core_types import SynergiaSession
+from .generic_types import SynergiaTeacher, SynergiaSubject, SynergiaLesson, SynergiaVirtualClass, SynergiaLessonEntry, SynergiaClassroom
 name = 'librus_tricks'
-
-
-def ez_login(email, passwd, rtype='UserObjs'):
-    """
-    Ułatwia pozyskanie danych do autoryzacji do librus synergia
-
-    :param email: Email do librus synergia
-    :type email: str
-    :type passwd: str
-    :param rtype: Wybiera w jaki co ma zostać zwrócone na return, wybieraj między `UserObjs` lub `tokens`
-    :type rtype: str
-    """
-    auth_code = get_auth_code(email, passwd)
-    librus_token = get_access_token(auth_code)
-    response = try_many_times(librus_token)
-    if rtype == 'UserObjs':
-        users = []
-        for a in response:
-            users.append(SynergiaSessionUser(a))
-        return users
-    elif rtype == 'tokens':
-        librus_tokens = {}
-        for t in response:
-            librus_tokens[t['studentName']] = t['accessToken']
-        return librus_token
-    else:
-        raise KeyError('Zły argument dla rtype')
