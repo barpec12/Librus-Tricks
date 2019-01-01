4f0a20137e9d3e06436d1ead22e1d39d0d08cbb9
Krystian <krystian@postek.eu>
2019-01-01 12:42:32 +0100

    Added new exception

--- librus_tricks/errors.py
+++ librus_tricks/errors.py
@@ -1,8 +1,14 @@
 class OverrideRequired(Exception):
     pass
 
+
 class WrongOption(Exception):
     pass
 
+
 class TokenExpired(Exception):
+    pass
+
+
+class ObjectNotFound(Exception):
     pass
