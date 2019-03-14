b8e793495476095e3ba66ce5bb3b955fa968c985
Backdoorek <krystian@postek.eu>
2019-03-14 16:09:33 +0100

    Initial commit
    Signed-off-by: Krystian <krystian@postek.eu>

--- librus_tricks/__init__.py
+++ librus_tricks/__init__.py
@@ -1,4 +1,4 @@
 from .utils import ez_login, convert_grade_keys
-from .core_types import SynergiaSession
-from .generic_types import SynergiaTeacher, SynergiaSubject, SynergiaLesson, SynergiaVirtualClass, SynergiaLessonEntry, SynergiaClassroom
+from .core_types_sync import SynergiaSession
+
 name = 'librus_tricks'
