47208290576920e4bd9b41c330a0df6f84edfa49
Krystian <krystian@postek.eu>
2019-03-20 19:25:05 +0100

    Created new exceptions

--- librus_tricks/exceptions.py
+++ librus_tricks/exceptions.py
@@ -8,3 +8,11 @@ class LibrusNotHandlerableError(Exception):
 
 class SynergiaEndpointNotFound(Exception):
     pass
+
+
+class LibrusWrongPasswordError(Exception):
+    pass
+
+
+class SynergiaEndpointRequireMorePermissions(Exception):
+    pass
