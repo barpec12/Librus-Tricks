d14fa43b681ce8dee846f644bc44ff1fdfbc7a99
Krystian <krystian@postek.eu>
2019-03-23 08:51:34 +0100

    Added new test

---
+++ tests/large_test.py
@@ -0,0 +1,86 @@
+import os
+import sys
+
+sys.path.extend(['./'])
+
+email = os.environ['librus_email']
+password = os.environ['librus_password']
+
+from librus_tricks import aio, SynergiaClient
+
+session = SynergiaClient(aio(email, password), cache_location='../librus_tricks/cache.sqlite')
+
+
+def test_grades():
+    grade = session.get_grades()
+    teachers = []
+    subjects = []
+    comments = []
+    categories = []
+    for g in grade:
+        teachers.append(
+            g.teacher
+        )
+        if not g.subject == None:
+            subjects.append(
+                g.subject
+            )
+        subjects.append(
+            g.comments
+        )
+        categories.append(
+            g.category
+        )
+
+    return grade, teachers, subjects, comments, categories
+
+
+def test_attendances():
+    atts = session.get_attendances()
+    teachers = []
+    types = []
+    for a in atts:
+        teachers.append(
+            a.teacher
+        )
+        types.append(
+            a.type
+        )
+
+
+def test_exams():
+    exams = session.get_exams()
+    teachers = []
+    subjects = []
+    groups = []
+    for e in exams:
+        teachers.append(
+            e.teacher
+        )
+        subjects.append(
+            e.subject
+        )
+        groups.append(
+            e.group
+        )
+
+def test_timetable():
+    week = session.get_timetable()
+    subjects = []
+    teachers = []
+    lessons = []
+    groups = []
+    for day in week.keys():
+        for frame in week[day]:
+            subjects.append(
+                frame.subject
+            )
+            teachers.append(
+                frame.teacher
+            )
+            lessons.append(
+                frame.lesson
+            )
+            groups.append(
+                frame.group
+            )
