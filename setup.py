a783ec157a0de584252fe458b43f69f4ac5c9d49
Krystian <krystian@postek.eu>
2019-03-28 19:06:46 +0100

    New tag yay

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="librus_tricks",
     python_requires='>3.6.0',
-    version="0.2.0",
+    version="0.2.1",
     author="Krystian Postek",
     author_email="krystian@postek.eu",
     description="A python wrapper of Synergia Librus API",
