86a1650766aa1e32374a4cd281a075ba5b1f5493
Krystian Postek <krystian@postek.eu>
2019-08-29 15:14:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="librus_tricks",
     python_requires='>3.6.0',
-    version="0.3.1",
+    version="0.4.0",
     author="Krystian Postek",
     author_email="krystian@postek.eu",
     description="A python wrapper of Synergia Librus API",
