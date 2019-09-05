c72f751af1fb28280eee5930f9f8fefa7fcb9a24
Krystian Postek <krystian@postek.eu>
2019-09-05 14:09:41 +0200

    Added util function

--- librus_tricks/__init__.py
+++ librus_tricks/__init__.py
@@ -2,3 +2,13 @@ name = 'librus_tricks'
 __title__ = 'librus_tricks'
 __author__ = 'Backdoorek'
 __version__ = 'alpha-0.0.1'
+
+from librus_tricks.auth import authorizer
+from librus_tricks.core import SynergiaClient
+from librus_tricks.classes import *
+
+
+async def create_session(email, password) -> SynergiaClient:
+    user = (await authorizer(email, password))[0]
+    session = SynergiaClient(user)
+    return session
